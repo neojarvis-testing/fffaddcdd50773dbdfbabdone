acb40da8b247283bc8aa027835f0b29d4af94683
neojarvis <techadmin@iamneo.ai>
2025-01-31 13:18:07 +0000

    initialCommit-Boilerplate

---
+++ pytest/run.sh
@@ -0,0 +1,4 @@
+cp /home/coder/project/workspace/pytest/tests.py /home/coder/project/workspace/project/tests.py
+cd /home/coder/project/workspace/project
+pip3 install pytest
+python3 -m pytest tests.py -v
